9b32eeefbc81c026b8a111f86af3f19f452c6ce1
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-09 15:30:14 -0400

    fix migration file

--- db/migrate/20230808194349_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users do |col|
-      col.string :name,
-      col.timestamps
-    end
-  end
-end
--- db/migrate/20230808201922_category_transaction.rb
+++
@@ -1,8 +0,0 @@
-class CategoryTransactions < ActiveRecord::Migration[7.0]
-  def change
-    create_table :category_transactions do |col|
-      col.references :category, null: false, foreign_key: true
-      col.references :transaction, null: false, foreign_key: true
-      col.timestamps
-  end
-end
