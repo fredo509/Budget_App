d3715ee4bf10d53dcdbb13f9f5f021c2d0897b93
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-10 12:06:57 -0400

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
   root "home_page#index"
-  resources :categories, do
+  resources :categories do
     resources :transactions, only: [:new, :create]
   end
 end
