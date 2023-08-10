6c36070f3bdd370e6bd71a90eaf074c984015c1c
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-09 21:53:17 -0400

    update association in model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :categories
-  has_many :categories, through: :category_id
+  has_many :category_transactions
+  has_many :categories, through: :category_transactions
 
   validates :name, presence: true
   validates :amount, presence: true
