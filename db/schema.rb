d45a4002c55ecd86a95e809af163824ad75f4d3e
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-10 13:08:32 -0400

    update shema

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_192017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categorie_transactions", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "transaction_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_categorie_transactions_on_category_id"
-    t.index ["transaction_id"], name: "index_categorie_transactions_on_transaction_id"
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
@@ -30,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_192017) do
     t.datetime "updated_at", null: false
     t.bigint "author_id"
     t.index ["author_id"], name: "index_categories_on_author_id"
+  end
+
+  create_table "category_transactions", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_transactions_on_category_id"
+    t.index ["transaction_id"], name: "index_category_transactions_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -47,8 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_192017) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "categorie_transactions", "categories"
-  add_foreign_key "categorie_transactions", "transactions"
   add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "category_transactions", "categories"
+  add_foreign_key "category_transactions", "transactions"
   add_foreign_key "transactions", "users", column: "author_id"
 end
