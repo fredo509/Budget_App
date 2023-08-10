d2646f49923f68dd197953b7c5740504f5fed384
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-10 12:59:54 -0400

    fix syntax error

--- db/migrate/20230809192017_create_categorie_transactions.rb
+++ db/migrate/20230809192017_create_categorie_transactions.rb
@@ -1,6 +1,6 @@
 class CreateCategorieTransactions < ActiveRecord::Migration[7.0]
   def change
-    create_table :categorie_transactions do |t|
+    create_table :category_transactions do |t|
       t.references :category, null: false, foreign_key: true
       t.references :transaction, null: false, foreign_key: true
 
