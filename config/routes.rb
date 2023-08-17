9efb57ce30451ff3a7d57d8a1dd79634b26643bc
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-17 17:50:11 -0400

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home_page#index"
+  resources :transactions
   resources :categories do
-    resources :transactions, only: [:new, :create]
+    resources :transactions, only: %i[index new create destroy update]
   end
 end
