b03736526485d8cc09468e541ea69cad383784e2
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-17 17:52:42 -0400

    update association for category and transaction

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :category_transactions
-  has_many :transactions, through: :category_transactions
+  has_many :transactions
 
   validates :name, presence: true
   validates :icon, presence: true
