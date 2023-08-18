bb5b587183c52e7828c498d6e896e803835154f2
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-17 23:14:27 -0400

    Update categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,45 +1,37 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
-  # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @user = current_user
+    @categories = @user.categories.includes(:transactions).order(id: :desc)
   end
 
-  # GET /categories/1 or /categories/1.json
   def show
     @category = Category.find(params[:id])
     if @category.author != current_user
-      flash[:alert] = 'Access Not Allowed !'
+      flash[:alert] = 'Not authorized dude!'
       redirect_to categories_path
     end
-    @transaction = @category.transactions.order(created_at: :desc)
-    @total_amount = @transaction.sum(:amount)
+    @transactions = Transaction.where(category_id: params[:id])
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
     @category.author = current_user
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created :)" }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+
+    if @category.save
+      flash[:notice] = 'Category successfully created'
+      redirect_to categories_path
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name, :icon)
-    end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
