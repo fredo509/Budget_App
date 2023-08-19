8b54e2cdbd858b447a2da41f194f7b9cca7dd4e4
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-18 23:23:17 -0400

    fix secret key

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module BudgetAppt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    validate_secret_key_base
+    config.secret_key_base = Rails.application.credentials.secret_key_base
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
